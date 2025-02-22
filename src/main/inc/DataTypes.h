98257524a65786fe0f8982de05c23d34b25fea94
kathy frantz <kfrantz21@gmail.com>
2025-02-21 23:36:48 -0600

    Created initial file structure and generic file templates

---
+++ src/main/inc/DataTypes.h
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////
+///  File: DataTypes.h
+///
+///  Overview: This h file creates standard data types with
+///     friendlier names
+///
+///////////////////////////////////////////////////////////////
+
+#ifndef DATATYPES_H_
+#define DATATYPES_H_
+
+// Includes ---------------------------------------------------
+
+#include <stdint.h>
+
+// Public Macros ----------------------------------------------
+
+// Public Type Defs -------------------------------------------
+
+typedef uint8_t U8;
+typedef int8_t S8;
+typedef uint16_t U16;
+typedef int16_t S16;
+typedef uint32_t U32;
+typedef int32_t S32;
+typedef float F32;
+
+// Public Var Defs --------------------------------------------
+
+// Public Func Defs -------------------------------------------
+
+
+#endif // DATATYPES_H_
